6f4ca2043f45dc27352442821fcf5a9f30ac05d4
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-12-10 15:28:29 +0100

    Solved: 9-2

--- day09/main.py
+++ day09/main.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python
 
 import fileinput
+import argparse
 
-DEBUG = False
+def compute_decompressed_length(data, recursive):
+    if not data:
+        return 0
+
+    if data[0] == '(':
+        # Convert '(AxB)' to [A, B]
+        spec_end_idx = data.find(')')
+        size_spec = data[1:spec_end_idx].split('x')
+        length = int(size_spec[0])
+        count = int(size_spec[1])
+
+        sub_data = data[spec_end_idx+1:spec_end_idx+length+1]
+        if '(' in sub_data and recursive:
+            data_length = compute_decompressed_length(sub_data, recursive)
+        else:
+            data_length = len(sub_data)
+
+        return count * data_length + compute_decompressed_length(data[spec_end_idx+length+1:], recursive)
+
+    if not '(' in data:
+        return len(data)
+
+    # Data is of the form X+(AxB)+X+. Find length of part before marker and decompress the rest
+    uncompressed_length = data.find('(')
+    return uncompressed_length + compute_decompressed_length(data[uncompressed_length:], recursive)
+
 
 if __name__ == '__main__':
-    total_length = 0
-    data = fileinput.input()[0].strip()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--part', default='1', type=int)
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args()
 
-    i = 0
-    while i < len(data):
-        if data[i] == '(':
-            # Convert '(AxB)' to [A, B]
-            spec_end_idx = data.find(')', i)
-            spec_str_length = spec_end_idx - i + 1
-            size_spec = data[i+1:spec_end_idx].split('x')
-            length = int(size_spec[0])
-            count = int(size_spec[1])
-
-            if DEBUG:
-                print(data[spec_end_idx+1:spec_end_idx+length+1]*count, end="")
-
-            total_length += length * count
-            i += spec_str_length + length
-        else:
-            if DEBUG:
-                print(data[i], end="")
-
-            total_length += 1
-            i += 1
-
-    print()
-    print(total_length)
+    for line in fileinput.input(args.files):
+        print(compute_decompressed_length(line.strip(), recursive=(args.part == 2)))
