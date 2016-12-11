31aeecd539c684dfb01386129c46191dc1165dca
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-12-11 18:11:52 +0100

    Solved: 11-1

--- day11/main.py
+++ day11/main.py
@@ -3,36 +3,47 @@
 import fileinput
 import re
 import time
+import itertools
 
 from collections import deque
+
+def log(s):
+    print(s)
+    #pass
+
+def get_component_type(component):
+    return component[1]
+
+def get_component_name(component):
+    return component[0]
+
+def is_microchip(component):
+    return get_component_type(component) == ComponentType.MICROCHIP
+
+def is_generator(component):
+    return get_component_type(component) == ComponentType.GENERATOR
+
+def get_generator(microchip):
+    return get_component_name(microchip) + ComponentType.GENERATOR
+
+def get_microchip(generator):
+    return get_component_name(generator) + ComponentType.MICROCHIP
 
 class State:
     def __init__(self, floor_count, components=None, elevator=1):
         self.__floor_count = floor_count
         self.__components = components if components else {}
+        self.__components_by_floor = self.__get_components_by_floor()
         self.__elevator = elevator
 
     def copy(self):
         return State(self.__floor_count, dict(self.__components), self.__elevator)
 
-    def is_same_state(self, other_state):
-        return self.__floor_count == other_state.__floor_count and \
-                self.__components == other_state.__components and \
-                self.__elevator == other_state.__elevator
-
     def add(self, component, floor):
         self.__components[component] = floor
+        self.__components_by_floor = self.__get_components_by_floor()
 
     def generate_next_states(self):
-        possible_payloads = []
-
-        components = self.__get_components_by_floor()[self.__elevator]
-        for idx, c in enumerate(components):
-            possible_payloads.append([c])
-
-            for c2 in components[idx+1:]:
-                possible_payloads.append([c, c2])
-
         possible_directions = []
         if self.__elevator < self.__floor_count:
             possible_directions.append(1)
@@ -40,52 +51,73 @@ class State:
         if self.__elevator > 1:
             possible_directions.append(-1)
 
-        moves = [Move(direction, payload) for payload in possible_payloads for direction in possible_directions]
-        res = []
+        moves = []
+        current_floor_components = self.__components_by_floor[self.__elevator]
+        for d, c1, c2 in itertools.product(possible_directions, current_floor_components, current_floor_components):
+            if c1 == c2:
+                moves.append(Move(d, [c1]))
+            else:
+                moves.append(Move(d, [c1, c2]))
 
+        seen_states = []
+
+        res = []
         for move in moves:
             new_state = self.copy()
             new_state.do_move(move)
-            if new_state.is_valid():
+            if new_state.is_valid() and new_state.__repr__() not in seen_states:
                 res.append((move, new_state))
-
+                seen_states.append(new_state.__repr__())
         return res
+
+    def is_valid(self):
+        for floor, components in self.__components_by_floor.items():
+            # Safe if floor has no components
+            if not components:
+                continue
+
+            # No generators on floor
+            if not self.__floor_has_generators(floor):
+                continue
+
+            # Otherwise, ensure each microchip has a corresponding RTG
+            for c in components:
+                if is_generator(c):
+                    continue
+
+                if not get_generator(c) in components:
+                    return False
+
+        return True
+
+    def __floor_has_generators(self, floor):
+        for c in self.__components_by_floor[floor]:
+            if is_generator(c):
+                return True
+        return False
 
     def do_move(self, move):
         self.__elevator += move.direction
         for c in move.components:
             self.__components[c] = self.__components[c] + move.direction
-
-    def is_valid(self):
-        for floor, components in self.__get_components_by_floor().items():
-            # Safe if floor has no chips
-            if not components:
-                continue
-
-            # Auto-safe if there are no generators
-            if not any(c[1] == 'G' for c in components):
-                continue
-
-            # Otherwise, ensure each microchip has a corresponding RTG
-            for c in components:
-                if c[1] == 'G':
-                    continue
-
-                required_rtg = c[0] + 'G'
-                if self.__components[required_rtg] != floor:
-                    return False
-
-        return True
+        self.__components_by_floor = self.__get_components_by_floor()
 
     def is_final_state(self):
-        components_by_floor = self.__get_components_by_floor()
+        floor_with_components_count = 0
+        for floor, components in self.__components_by_floor.items():
+            if components and not floor == self.__floor_count:
+                return False
 
-        return len(components_by_floor) == 1 and self.__floor_count in components_by_floor
+        return True
 
     def __get_components_by_floor(self):
         components_by_floor = {}
         for c, floor in self.__components.items():
             components_by_floor.setdefault(floor, []).append(c)
+
+        for floor in range(1, self.__floor_count + 1):
+            components_by_floor.setdefault(floor, [])
+
         return components_by_floor
 
 
@@ -117,8 +149,9 @@ class ComponentType:
     MICROCHIP = 'M'
 
 class Node:
-    def __init__(self, state, parent_node=None):
+    def __init__(self, state, move=None, parent_node=None):
         self.state = state
+        self.move = move
         self.parent_node = parent_node
 
 def main():
@@ -145,6 +178,12 @@ def main():
 
          'curium': 'C',
          'curium-compatible': 'C',
+
+         'cobalt': 'N',
+         'cobalt-compatible': 'N',
+
+         'promethium': 'Q',
+         'promethium-compatible': 'Q'
     }
 
     component_type_to_letter = {
@@ -171,42 +210,71 @@ def main():
                 component = element_to_letter[line_parts[idx - 1]] + component_type_to_letter[line_part]
                 initial_state.add(component, floor)
 
+    #initial_state = State(4, elevator=3)
+    #initial_state.add('HG', 4)
+    #initial_state.add('HM', 3)
+    #initial_state.add('LG', 4)
+    #initial_state.add('LM', 3)
+
     node_queue = deque()
     node_queue.append(Node(initial_state))
 
     visited_states = []
 
+    print(initial_state)
+
+    final_node = None
     while node_queue:
         parent_node = node_queue.popleft()
-        visited_states.append(parent_node.state)
 
-        #print(len(node_queue))
-        #print(parent_node.state, flush=True)
-        #time.sleep(0.5)
+        if parent_node.state.__repr__() in visited_states:
+            continue
+
+        visited_states.append(parent_node.state.__repr__())
+
+        print(len(visited_states))
+        print(len(unwind_path(parent_node)), flush=True)
+
+        #print('Checking state')
+        #print(parent_node.state)
+        #print('Is final: %s' % parent_node.state.is_final_state(), flush=True)
+        #time.sleep(1)
 
         if parent_node.state.is_final_state():
-            step_count = 0
-            temp_node = parent_node
-            while temp_node.parent_node:
-                temp_node = temp_node.parent_node
-                step_count += 1
-
-            print(parent_node.state)
-            print(str(step_count))
+            final_node = parent_node
             break
 
         for move, state in parent_node.state.generate_next_states():
-            skip = False
-            for visited in visited_states:
-                if state.is_same_state(visited):
-                    skip = True
-                    break
+            if not state.__repr__() in visited_states:
+                node_queue.append(Node(state, move, parent_node))
 
-            if skip:
-                continue
+    if final_node:
+        step_count = 0
+        node_path = []
+        node_path.append(final_node)
+        
+        temp_node = parent_node
+        while temp_node.parent_node:
+            temp_node = temp_node.parent_node
+            step_count += 1
+            node_path.append(temp_node)
 
-            node_queue.append(Node(state, parent_node))
+        for node in reversed(node_path):
+            log(node.move)
+            log(node.state)
 
+        log(len(node_path) - 1)
+
+
+def unwind_path(end_node):
+    node_path = []
+    node_path.append(end_node)
+    
+    temp_node = end_node
+    while temp_node.parent_node:
+        temp_node = temp_node.parent_node
+        node_path.append(temp_node)
+    return node_path
 
 if __name__ == '__main__':
     main()
---
+++ day11/main2.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import fileinput
+import re
+import time
+import itertools
+
+from collections import deque
+
+#Initial state
+#F4 . .  .  .  .  .  .  .  .  .  .
+#F3 . .  .  .  .  .  .  .  .  .  TM
+#F2 . CG CM .  .  RG RM .  .  TG .
+#F1 E .  .  PG PM .  .  SG SM .  .
+
+COMPONENTS = ['HG', 'HM', 'LG', 'LM']
+COMPONENTS = ['CG', 'CM', 'PG', 'PM', 'RG', 'RM', 'SG', 'SM', 'TG', 'TM']
+
+def log(s):
+    print(s)
+
+class ComponentType:
+    GENERATOR = 'G'
+    MICROCHIP = 'M'
+
+class Component:
+    def __init__(self, name, component_type):
+        self.name = name
+        self.type = component_type
+
+    def __repr__(self):
+        return '%s%s' % (self.name, self.type)
+
+class State:
+    def __init__(self, floor_count, components, elevator=1):
+        self.__floor_count = floor_count
+        self.__components = components
+        self.__elevator = elevator
+
+    def copy(self):
+        return State(self.__floor_count, list(self.__components), self.__elevator)
+
+    def generate_next_states(self):
+        possible_directions = []
+        if self.__elevator < self.__floor_count:
+            possible_directions.append(1)
+
+        if self.__elevator > 1:
+            possible_directions.append(-1)
+
+        components_indices_on_floor = [idx for idx, floor in enumerate(self.__components) if floor == self.__elevator]
+        for d, idx1, idx2 in itertools.product(possible_directions, components_indices_on_floor, components_indices_on_floor):
+            new_components = list(self.__components)
+            new_components[idx1] += d
+            if idx1 != idx2:
+                new_components[idx2] += d
+
+            new_state = State(self.__floor_count, new_components, self.__elevator + d)
+            if new_state.__is_valid():
+                yield (Move(d), new_state)
+
+    def __is_valid(self):
+        generator_floors = self.__components[::2]
+        for idx, chip_floor in enumerate(self.__components):
+            if idx % 2 == 0:
+                # Generator
+                continue
+
+            if chip_floor in generator_floors and not self.__components[idx - 1] == chip_floor:
+                return False
+
+        return True
+
+    def is_final_state(self):
+        return all(c == self.__floor_count for c in self.__components)
+
+    def hash(self):
+        component_pairs = list(zip(self.__components[::2], self.__components[1::2]))
+        return {'e': self.__elevator, 'c': sorted(component_pairs)}
+
+    def __repr__(self):
+        res = ''
+        for floor in range(self.__floor_count, 0, -1):
+            res += 'F' + str(floor).zfill(len(str(self.__floor_count)))
+            res += ' '
+            res += 'E' if self.__elevator == floor else '.'
+            res += ' '
+            
+            for idx, f in enumerate(self.__components):
+                res += COMPONENTS[idx] if f == floor else '. '
+                res += ' '
+
+            res += '\n'
+        return res
+
+class Move:
+    def __init__(self, direction):
+        self.direction = direction
+
+    def __repr__(self):
+        return '%s' % (self.direction)
+
+class Node:
+    def __init__(self, state, move=None, parent_node=None):
+        self.state = state
+        self.move = move
+        self.parent_node = parent_node
+
+def main():
+    floor_count = 4
+    components = [3, 3, 1, 2, 3, 3, 1, 2, 1, 1]
+    components = [4, 4, 4, 1]
+    components = [2, 1, 3, 1]
+    components = [2, 2, 1, 1, 2, 2, 1, 1, 2, 3]
+    elevator = 1
+
+    initial_state = State(floor_count, components, elevator)
+    print(initial_state)
+
+    node_queue = deque()
+    node_queue.append(Node(initial_state))
+
+    test_state = State(4, [4, 1, 4, 4], 1)
+    print(initial_state.hash() == test_state.hash())
+    print(initial_state.hash())
+    print(test_state.hash())
+
+    visited_states = []
+    visited_states.append(initial_state.hash())
+
+    final_node = None
+    while node_queue:
+        parent_node = node_queue.popleft()
+        #visited_states.append(parent_node.state.hash())
+        print(len(unwind_path(parent_node)))
+        print(len(node_queue), flush=True)
+
+        if parent_node.state.is_final_state():
+            final_node = parent_node
+            break
+
+        for move, state in parent_node.state.generate_next_states():
+            if not state.hash() in visited_states:
+                node_queue.append(Node(state, move, parent_node))
+                visited_states.append(state.hash())
+                print(state)
+                print(state.hash())
+
+    if final_node:
+        step_count = 0
+        node_path = []
+        node_path.append(final_node)
+        
+        temp_node = parent_node
+        while temp_node.parent_node:
+            temp_node = temp_node.parent_node
+            step_count += 1
+            node_path.append(temp_node)
+
+        for node in reversed(node_path):
+            log(node.move)
+            log(node.state)
+
+        log(len(node_path) - 1)
+
+
+def unwind_path(end_node):
+    node_path = []
+    node_path.append(end_node)
+    
+    temp_node = end_node
+    while temp_node.parent_node:
+        temp_node = temp_node.parent_node
+        node_path.append(temp_node)
+    return node_path
+
+if __name__ == '__main__':
+    main()
