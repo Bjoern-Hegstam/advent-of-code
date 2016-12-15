315d6794d6cd25b0ada27dd71b65b0e9b5bcebb4
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-12-15 20:44:00 +0100

    Solved: 15-1

--- day15/main.py
+++ day15/main.py
@@ -10,29 +10,13 @@ class Disc:
         self.p0 = p0
         self.N = N
 
+    def get_pos(self, t):
+        return (self.p0 + t) % self.N
+
+    def __repr__(self):
+        return 'Disc #{0} has {1} positions; at time=0, it is at position {2}.'.format(self.h, self.N, self.p0)
+
 def main(part, files):
-    '''
-    t = time
-    h = disc height/number
-    N = Number of positions
-    p0 = Initial position
-
-    pos(t) = h + p0 + t (mod N)
-
-    Ball can pass at time t if pos(t) = 0
-
-    0 = 1 + 4 + t + 5*a
-    0 = 2 + 1 + t + 2*b
-
-    =>
-
-    0 = 5 + t + 5*a
-    0 = 3 + t + 2*b
-
-    0 | 5 5 0
-    0 | 3 0 2
-    '''
-
     discs = []
     lines = [line.strip() for line in fileinput.input(files)]
 
@@ -40,13 +24,42 @@ def main(part, files):
         vals = re.match(r'^Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+)\.$', line)
         params = [int(v) for v in vals.groups()]
         discs.append(Disc(params[0], params[2], params[1]))
-        
-    # Print equation system
-    variables = 'abcdefghijklmnopqrstuvxyz'
+
+    print_equation_system(discs)
+
+    #print('Drop time: {}'.format(find_drop_time(discs)))
+
+
+def print_equation_system(discs):
+    varibles = 'abcdefghiklmnopqstuvxyz'
     for disc in discs:
-        print('0 = ', end='')
-        print('{:2d}'.format(disc.h + disc.p0), end='')
-        print('+ t + {0:2d}{1}'.format(disc.N, variables[disc.h - 1]), end=', ')
+        print('0 = {0:2d} + t + {1:2d}{2}'.format(disc.h + disc.p0, disc.N, varibles[disc.h - 1]), end=', ')
+
+
+def find_drop_time(discs):
+    drop_time = 0
+    t = 0
+    ball_pos = 0
+
+    while True:
+        t += 1
+        ball_pos += 1
+        print('\nt({0}) - B({1})'.format(t, ball_pos))
+
+        for disc in discs:
+            print('D({0}) - {1}'.format(disc.h, disc.get_pos(t)))
+
+        if discs[ball_pos - 1].get_pos(t) != 0:
+            ball_pos = 0
+            drop_time = t
+            continue
+
+        if ball_pos == len(discs):
+            break
+
+        ball_pos
+
+    return drop_time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
