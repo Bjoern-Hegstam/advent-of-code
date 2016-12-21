df3a4794cba2d5d1eb8dd1859f2f47df7600641f
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-12-21 21:29:22 +0100

    Implement letter swap and rotation

--- day21/main.py
+++ day21/main.py
@@ -6,20 +6,54 @@ import re
 
 DEBUG = False
 
-class SwapOperation:
+class SwapPositions:
     def __init__(self):
         self.pattern = re.compile(r'^swap position (?P<x>\d+) with position (?P<y>\d+)$')
 
     def run(self, match, password):
         x = int(match.group('x'))
         y = int(match.group('y'))
-        password[x], password[y] = password[y], password[x]        
 
+        new_pass = password.copy()
+        new_pass[x], new_pass[y] = password[y], password[x]
+        return new_pass
+
+class SwapLetters:
+    def __init__(self):
+        self.pattern = re.compile(r'swap letter (?P<cx>\w) with letter (?P<cy>\w)$')
+
+    def run(self, match, password):
+        cx = match.group('cx')
+        cy = match.group('cy')
+
+        x = password.index(cx)
+        y = password.index(cy)
+
+        new_pass = password.copy()
+        new_pass[x], new_pass[y] = password[y], password[x]
+        return new_pass
+
+class RotateFixedDirection:
+    def __init__(self):
+        self.pattern = re.compile(r'rotate (?P<dir>left|right) (?P<num_steps>\d+) steps?$')
+
+    def run(self, match, password):
+        print('Match')
+        direction = 1 if match.group('dir') == 'left' else -1
+        num_steps = int(match.group('num_steps'))
+
+        move = direction*num_steps % len(password)
+        print(move)
+
+        new_pass = password[move:] + password[:move]
+        return new_pass
 
 
 def main(files, part='1'):
     operations = [
-        SwapOperation()
+        SwapPositions(),
+        SwapLetters(),
+        RotateFixedDirection()
     ]
 
     password = list('abcde')
@@ -28,7 +62,7 @@ def main(files, part='1'):
         for op in operations:
             match = op.pattern.match(line)
             if match:
-                op.run(match, password)
+                password = op.run(match, password)
                 break
 
 
