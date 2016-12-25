1091ae27db69f5d5df2dc30a59fc6076d75a2faf
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-12-25 12:18:09 +0100

    Parse nodes

--- day22/main.py
+++ day22/main.py
@@ -1,21 +1,42 @@
-
 #!/usr/bin/env python
 
 import fileinput
 import argparse
 import re
-import itertools
 
 DEBUG = False
 
 
-def main(files, part='1'):
-    input_lines = [line.strip() for line in fileinput.input(files)]
+class Node:
+    def __init__(self, x, y, size, used, avail, use):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.used = used
+        self.avail = avail
+        self.use = use
 
-    if part == '1':
-        pass
-    elif part == '2':
-        pass
+
+def main(files, part='1'):
+    node_pattern = re.compile(r'^/dev/grid/node-x(?P<x>\d+)-y(?P<y>\d+)\s+(?P<size>\d+)T\s+(?P<used>\d+)T\s+(?P<avail>\d+)T\s+(?P<use>\d+)%$')
+
+    nodes = []
+
+    for line in fileinput.input(files):
+        match = node_pattern.match(line)
+        if not match:
+            continue
+
+        nodes.append(Node(
+                match.group('x'),
+                match.group('y'),
+                match.group('size'),
+                match.group('used'),
+                match.group('avail'),
+                match.group('used')
+            ))
+
+
 
 
 if __name__ == '__main__':
