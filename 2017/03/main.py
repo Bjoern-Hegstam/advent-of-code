459925cfb0bd5c419a8ec26f2aeae92fdd2ed6a7
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-12-04 11:23:07 +0100

    Solved 2017 day 3 part 2
    Also rewrote part 1 to use a brute force approach. Ends up being much simpler to reason about and works without problem for the spiral size used.

--- 2017/03/main.py
+++ 2017/03/main.py
@@ -1,10 +1,4 @@
-import math
 import logging
-
-# Ring index = n
-# Side length L = 2*n + 1
-# Max number in ring = L^2
-# Number of numbers per ring T = 2*L + 2*(L - 2) = 4*n + 2 + 4*n + 2 - 4 = 8*n
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -12,54 +6,73 @@ logger = logging.getLogger(__name__)
 
 def main():
     print('Answer part one: {}'.format(solve_part_one(347991)))
+    print('Answer part two: {}'.format(solve_part_two(347991)))
 
 
 def solve_part_one(val):
-    logging.debug('Input val: {}'.format(val))
+    grid = generate_spiral(val)
+    for pos, v in grid.items():
+        if v == val:
+            return abs(pos[0]) + abs(pos[1])
+    return -1
 
-    if val == 1:
-        return 0
 
-    # Find which ring the input is in
-    side_length = math.ceil(math.sqrt(val))
-    if side_length % 2 == 0:
-        side_length += 1
+def generate_spiral(max_val, val_fun=lambda grid, prev_num, x, y: prev_num + 1):
+    x = 0
+    y = 0
+    grid = {}
 
-    n = int((side_length - 1) / 2)
+    direction = (1, 0)
 
-    logger.debug('side_length: {}'.format(side_length))
-    logger.debug('n: {}'.format(n))
+    n = 0
+    num = 1
+    current_val = 1
+    prev_val = 0
 
-    # Calculate interval and center number for each side
-    prev_max_num = (2 * (n - 1) + 1) ** 2
-    sides = {
-        (prev_max_num + 1, prev_max_num + 2 * n, prev_max_num + n),  # (min num, max num, center)
-        (prev_max_num + 2 * n, prev_max_num + 4 * n, prev_max_num + 3 * n),
-        (prev_max_num + 4 * n, prev_max_num + 6 * n, prev_max_num + 5 * n),
-        (prev_max_num + 6 * n, prev_max_num + 8 * n, prev_max_num + 7 * n)
-    }
+    while prev_val <= max_val:
+        grid[(x, y)] = current_val
 
-    logger.debug(prev_max_num)
-    logger.debug(sides)
+        if x == n and y == -n:
+            n += 1
 
-    # Find which side the number is in and calculate distance from center
-    dist = -1
-    for min_num, max_num, center in sides:
-        if min_num <= val <= max_num:
-            dist = abs(center - val)
-            break
-    logger.debug('dist: {}'.format(dist))
+        if direction[0] == 1 and x == n:
+            direction = rot(direction)
+        elif direction[0] == -1 and x == -n:
+            direction = rot(direction)
+        elif direction[1] == 1 and y == n:
+            direction = rot(direction)
+        elif direction[1] == -1 and y == -n:
+            direction = rot(direction)
 
-    # Add on distance to get to the ring from center in one direction
-    dist += n
-    logger.debug('Total dist: {}'.format(dist))
-    return dist
+        x, y = x + direction[0], y + direction[1]
+
+        prev_val = current_val
+        current_val = val_fun(grid, num, x, y)
+        num += 1
+    return grid
+
+
+def rot(v):
+    return -v[1], v[0]
 
 
 assert solve_part_one(1) == 0
 assert solve_part_one(12) == 3
 assert solve_part_one(23) == 2
 assert solve_part_one(1024) == 31
+
+
+def solve_part_two(val):
+    grid = generate_spiral(val, lambda g, prev_num, x, y: area_sum(g, x, y))
+    return max(grid.values())
+
+
+def area_sum(grid, x0, y0):
+    tot = 0
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            tot += grid.get((x0 + dx, y0 + dy), 0)
+    return tot
 
 
 if __name__ == '__main__':
