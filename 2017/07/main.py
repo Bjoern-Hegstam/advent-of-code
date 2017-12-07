4965fe4ae5421e2906f8dacbfea6047425275797
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-12-07 11:01:50 +0100

    Solved 2017 day 7 part 2

--- 2017/07/main.py
+++ 2017/07/main.py
@@ -4,13 +4,14 @@ import re
 def main():
     with open('input') as f:
         lines = f.readlines()
-    print('Answer part 1: {}'.format(solve_part_one(lines)))
-
-
-def solve_part_one(lines):
     programs = build_program_tree(lines)
 
-    bottoms = [name for name, data in programs.items() if not data.parent]
+    print('Answer part 1: {}'.format(solve_part_one(programs)))
+    print('Answer part 2: {}'.format(solve_part_two(programs)))
+
+
+def solve_part_one(programs):
+    bottoms = [name for name, program in programs.items() if not program.parent]
     assert len(bottoms) == 1
     return bottoms[0]
 
@@ -22,22 +23,80 @@ def build_program_tree(lines):
         if match:
             name = match.group('name')
             weight = int(match.group('weight'))
-            p = programs.setdefault(name, ProgramData())
+            p = programs.setdefault(name, ProgramData(name))
             p.weight = weight
 
             if match.groupdict()['children']:
-                for child in match.group('children').split(', '):
-                    programs.setdefault(child, ProgramData()).parent = name
+                for child_name in match.group('children').split(', '):
+                    child = programs.setdefault(child_name, ProgramData(child_name))
+                    child.parent = p
+                    p.children.append(child)
     return programs
 
 
+def solve_part_two(programs):
+    root = [p for p in programs.values() if not p.parent][0]
+
+    disc_weights = {}
+
+    calc_disc_weights(root, disc_weights)
+
+    current_program = root
+    while True:
+        unbalanced_child, expected_weight = find_unbalanced(current_program.children, disc_weights)
+        if not unbalanced_child.children:
+            return expected_weight
+
+        unbalanced_grandchild, _ = find_unbalanced(unbalanced_child.children, disc_weights)
+        if not unbalanced_grandchild:
+            return expected_weight - sum(disc_weights[c.name] for c in unbalanced_child.children)
+        else:
+            current_program = unbalanced_child
+
+
+def calc_disc_weights(program, disc_weights):
+    if program.name in disc_weights:
+        return disc_weights[program.name]
+
+    if program.children:
+        child_weights = sum(calc_disc_weights(child, disc_weights) for child in program.children)
+    else:
+        child_weights = 0
+
+    return disc_weights.setdefault(
+        program.name,
+        program.weight + child_weights
+    )
+
+
+def find_unbalanced(programs, disc_weights):
+    weights = [(p, disc_weights[p.name]) for p in programs]
+    disc_weight_hist = {}
+    for p, disc_weight in weights:
+        disc_weight_hist[disc_weight] = disc_weight_hist.get(disc_weight, 0) + 1
+
+    if len(disc_weight_hist) == 1:
+        return None, weights[0][1]
+
+    for p, disc_weight in weights:
+        if disc_weight_hist[disc_weight] == 1:
+            return p, max(disc_weight_hist, key=disc_weight_hist.get)
+
+
 class ProgramData:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.weight = None
         self.parent = None
+        self.children = []
 
     def __repr__(self) -> str:
-        return 'ProgramData(weight={}, parent={})'.format(self.weight, self.parent)
+        return 'ProgramData(name={}, weight={}, parent={}, children=[{})'.format(
+            self.name,
+            self.weight,
+            self.parent,
+            ', '.join([p.name for p in self.children])
+        )
 
 
 test_input = """pbga (66)
@@ -54,7 +113,11 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)"""
 
-assert solve_part_one(test_input.splitlines()) == 'tknk'
+test_programs = build_program_tree(test_input.splitlines())
+assert solve_part_one(test_programs) == 'tknk'
+
+assert solve_part_two(test_programs) == 60
+
 
 if __name__ == '__main__':
     main()
