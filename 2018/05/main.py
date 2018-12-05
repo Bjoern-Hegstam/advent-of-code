aaa8e2cbcef2ba2a730a1c24eeb5ef84c2ea251d
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-05 21:32:21 +0100

    Solved 2018 day 5 part 2

--- 2018/05/main.py
+++ 2018/05/main.py
@@ -3,39 +3,46 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def main():
     with open('input') as f:
-        base_line = f.readline()
+        base_polymer = f.readline()
 
-    reduced_polymer = reduce_polymer(base_line)
+    reduced_polymer = react_polymer(base_polymer)
 
     print('Answer part 1: {}'.format(len(reduced_polymer)))
 
+    reacted_lengths = {}
+    for unit_pair in zip(alphabet, alphabet.upper()):
+        print('Removing unit pair {}'.format(unit_pair))
+        polymer = base_polymer.replace(unit_pair[0], '').replace(unit_pair[1], '')
+        length = len(react_polymer(polymer))
+        reacted_lengths[unit_pair] = length
 
-def reduce_polymer(base_line):
+        print('Removing unit pair {} gave length {}'.format(unit_pair, length))
+
+    print('Answer part 2: {}'.format(min(reacted_lengths.values())))
+
+
+def react_polymer(source_polymer):
     unit_pairs = list(zip(alphabet, alphabet.upper())) + list(zip(alphabet.upper(), alphabet))
 
-    line = base_line
+    print('Reacting polymer')
+    polymer = list(source_polymer)
     while True:
-        new_line = ''
+        polymer_reacted = False
 
         i = 0
-        while i < len(line) - 1:
-            if (line[i], line[i + 1]) in unit_pairs:
-                i += 2
+        while i < len(polymer) - 1:
+            if (polymer[i], polymer[i + 1]) in unit_pairs:
+                del polymer[i:i+2]
+                polymer_reacted = True
                 continue
+            else:
+                i += 1
 
-            new_line += line[i]
-            i += 1
-
-        if i == len(line) - 1:
-            new_line += line[-1]
-
-        if new_line != line:
-            line = new_line
-        else:
-            return new_line
+        if not polymer_reacted:
+            return polymer
 
 
-assert reduce_polymer('dabAcCaCBAcCcaDA') == 'dabCBAcaDA'
+assert react_polymer('dabAcCaCBAcCcaDA') == list('dabCBAcaDA')
 
 
 if __name__ == '__main__':
