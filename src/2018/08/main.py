da4a05a326eafb49c5ffdd3b6e57aa86cb660436
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-08 14:16:36 +0100

    Refactor

--- src/2018/08/main.py
+++ src/2018/08/main.py
@@ -10,26 +10,31 @@ def main():
 
     tree_definition = [int(n) for n in line.split()]
 
-    metadata_count = count_metadata(tree_definition)
+    root_node, nodes = build_tree(tree_definition)
+
+    metadata_count = count_metadata(nodes)
     assert metadata_count == 45868
     print('Answer part 1: {}'.format(metadata_count))
 
 
-def count_metadata(tree_definition):
-    first_node = Node(0, Header(tree_definition[0], tree_definition[1]), [], [])
+def count_metadata(nodes):
+    return sum(sum(node.metadata) for node in nodes.values())
 
-    nodes = {first_node.name: first_node}
 
-    commands = list(generate_node_commands(first_node))
+def build_tree(tree_definition):
+    root_node = Node(0, Header(tree_definition[0], tree_definition[1]), [], [])
+
+    nodes = {root_node.name: root_node}
+    commands = list(generate_node_commands(root_node))
+
     idx = 2
-
-    # Parse tree definition
     while commands:
         command = commands.pop()
         if command[0] == 'parse_child':
-            child_node = Node(len(nodes), Header(tree_definition[idx], tree_definition[idx + 1],), [], [])
+            child_node = Node(len(nodes), Header(tree_definition[idx], tree_definition[idx + 1], ), [], [])
             commands.extend(generate_node_commands(child_node))
             nodes[child_node.name] = child_node
+            nodes[command[1].name].children.append(child_node)
             idx += 2
         else:
             assert command[0] == 'parse_metadata'
@@ -37,7 +42,7 @@ def count_metadata(tree_definition):
             node.metadata.extend(tree_definition[idx:idx + node.header.metadata_count])
             idx += node.header.metadata_count
 
-    return sum(sum(node.metadata) for node in nodes.values())
+    return root_node, nodes
 
 
 def generate_node_commands(node):
@@ -46,7 +51,7 @@ def generate_node_commands(node):
         yield 'parse_child', node
 
 
-assert count_metadata([2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]) == 138
+assert count_metadata(build_tree([2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2])[1]) == 138
 
 if __name__ == '__main__':
     main()
