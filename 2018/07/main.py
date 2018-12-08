3e21f62b95415b471566db3cae3326e9eb404851
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-08 13:20:53 +0100

    Solved 2018 day 7 part 2

--- 2018/07/main.py
+++ 2018/07/main.py
@@ -1,6 +1,10 @@
 from collections import namedtuple
+from operator import attrgetter
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 StepRequirement = namedtuple('StepRequirement', 'req, step')
+WorkItem = namedtuple('WorkItem', 'step, finished_at')
 
 
 def main():
@@ -8,38 +12,57 @@ def main():
         lines = f.readlines()
 
     step_requirements = [StepRequirement(line[5], line[36]) for line in lines]
-    step_order = determine_step_order(step_requirements)
 
-    print('Answer part 1: {}'.format(step_order))
+    part_one_answer = determine_step_order(step_requirements)
+    assert part_one_answer == ('CGKMUWXFAIHSYDNLJQTREOPZBV', 0)
+    print('Answer part 1: {}'.format(part_one_answer))
+
+    answer_part_two = determine_step_order(step_requirements, worker_count=5, processing_takes_time=True, base_processing_time=60)
+    assert answer_part_two == ('CGUXKMFSWAIHYDNQLJTREOPZBV', 1046)
+    print('Answer part 2: {}'.format(answer_part_two))
 
 
-def determine_step_order(individual_step_requirements):
+def determine_step_order(step_requirements, worker_count=1, processing_takes_time=False, base_processing_time=0):
+    requirements_per_step = {}
+    for step_req in step_requirements:
+        requirements_per_step.setdefault(step_req.step, set()).add(step_req.req)
+
+    all_steps = set(step_req.req for step_req in step_requirements).union(step_req.step for step_req in step_requirements)
+    steps_that_can_be_started = [step for step in all_steps if step not in requirements_per_step]
+
+    current_time = 0
+    current_work_items = []
     step_order = []
-    taken_steps = set()
-
-    step_requirements = {}
-    for step_req in individual_step_requirements:
-        step_requirements.setdefault(step_req.step, set()).add(step_req.req)
-
-    all_steps = set(step_req.req for step_req in individual_step_requirements).union(step_req.step for step_req in individual_step_requirements)
-    available_steps = [step for step in all_steps if step not in step_requirements]
 
     while len(step_order) < len(all_steps):
-        available_steps.sort()
+        steps_that_can_be_started.sort(reverse=True)
 
-        current_step = available_steps.pop(0)
-        step_order.append(current_step)
-        taken_steps.add(current_step)
+        # Assign available workers
+        while worker_count - len(current_work_items) > 0 and steps_that_can_be_started:
+            next_step = steps_that_can_be_started.pop()
+            current_work_items.append(
+                WorkItem(
+                    next_step,
+                    current_time + base_processing_time + 1 + alphabet.find(next_step) if processing_takes_time else 0
+                )
+            )
 
-        for step, reqs in step_requirements.items():
-            if step in taken_steps:
-                continue
-            if step in available_steps:
-                continue
-            if taken_steps.issuperset(reqs):
-                available_steps.append(step)
+        # Move forward to next time step when a work item is finished
+        current_work_items.sort(key=attrgetter('finished_at'), reverse=True)
+        current_time = current_work_items[-1].finished_at
+        while current_work_items and current_work_items[-1].finished_at == current_time:
+            work_item = current_work_items.pop()
+            step_order.append(work_item.step)
 
-    return ''.join(step_order)
+        # Update list of steps that can be started
+        reserved_steps = set(step_order).union(steps_that_can_be_started).union(wi.step for wi in current_work_items)
+        for step, reqs in requirements_per_step.items():
+            if step not in reserved_steps and set(step_order).issuperset(reqs):
+                steps_that_can_be_started.append(step)
+
+    result = ''.join(step_order), current_time
+    print('DEBUG: Step order: {}'.format(result))
+    return result
 
 
 assert determine_step_order([
@@ -50,7 +73,17 @@ assert determine_step_order([
     StepRequirement('B', 'E'),
     StepRequirement('D', 'E'),
     StepRequirement('F', 'E')
-]) == 'CABDFE'
+]) == ('CABDFE', 0)
+
+assert determine_step_order([
+    StepRequirement('C', 'A'),
+    StepRequirement('C', 'F'),
+    StepRequirement('A', 'B'),
+    StepRequirement('A', 'D'),
+    StepRequirement('B', 'E'),
+    StepRequirement('D', 'E'),
+    StepRequirement('F', 'E')
+], worker_count=2, processing_takes_time=True, base_processing_time=0) == ('CABFDE', 15)
 
 if __name__ == '__main__':
     main()
