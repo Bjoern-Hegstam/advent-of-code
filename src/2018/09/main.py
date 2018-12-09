306431a1a30a163cfb4abdc56efedaea0dec4065
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-09 10:16:13 +0100

    Solved 2018 day 9 part 2
    Part 2 was not solvable in a reasonable time with previous approach. Using a deque + rotate works better and also made the code much easier to understand.

--- src/2018/09/main.py
+++ src/2018/09/main.py
@@ -1,47 +1,32 @@
-DEBUG = False
+from collections import deque
 
 
 def main():
-    player_count = 438
-    marble_count = 71626
+    high_score_part_one = play_marble_game(438, 71626)
+    assert high_score_part_one == 398730
+    print('Answer part 1: {}'.format(high_score_part_one))
 
-    high_score = play_marble_game(player_count, marble_count)
-    print('Answer part 1: {}'.format(high_score))
+    high_score_part_two = play_marble_game(438, 7162600)
+    assert high_score_part_two == 3349635509
+    print('Answer part 2: {}'.format(high_score_part_two))
 
 
 def play_marble_game(player_count, marble_count):
-    marbles = [0]
+    marbles = deque()
+    marbles.append(0)
     next_marble = 1
-    current_marble_idx = 0
     scores = {}
 
     print('Playing game: {} players, {} marbles'.format(player_count, marble_count))
 
     player_idx = 0
     while next_marble <= marble_count:
-        if DEBUG:
-            print('Current marble is {} at index {}, next marble is {}'.format(marbles[current_marble_idx], current_marble_idx, next_marble))
-
         if next_marble % 23 == 0:
-            next_marble_idx = (current_marble_idx - 7) % len(marbles)
-            marble_to_delete = marbles[next_marble_idx]
-
-            if DEBUG:
-                print('Found multiple of 23: {}, deleting marble {} at index {}'.format(next_marble, marble_to_delete, next_marble_idx))
-
-            scores[player_idx] = scores.get(player_idx, 0) + next_marble + marble_to_delete
-            del marbles[next_marble_idx]
-            current_marble_idx = next_marble_idx
+            marbles.rotate(-7)
+            scores[player_idx] = scores.get(player_idx, 0) + next_marble + marbles.pop()
         else:
-            next_marble_idx = current_marble_idx + 2
-            if next_marble_idx == len(marbles) + 1:
-                next_marble_idx = 1
-
-            if DEBUG:
-                print('Adding marble {} at index {}'.format(next_marble, next_marble_idx))
-
-            marbles.insert(next_marble_idx, next_marble)
-            current_marble_idx = next_marble_idx
+            marbles.rotate(2)
+            marbles.append(next_marble)
 
         player_idx = (player_idx + 1) % player_count
         next_marble += 1
