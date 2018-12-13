9a3a223acd69938e7c45d96f804d957372881e16
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-13 21:07:15 +0100

    Add comment to clarify purpose of rail direction data structure

--- src/2018/13/main.py
+++ src/2018/13/main.py
@@ -14,6 +14,7 @@ class Directions:
     RIGHT = Vector2(1, 0)
 
 
+# Map train direction when entering the rail to exit direction
 RAIL_DIRECTIONS_PER_CHAR = {
     '/': {
         Directions.UP: Directions.RIGHT,
