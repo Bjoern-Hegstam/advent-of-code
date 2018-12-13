4062200a04fa2d4d4aaa4334191ecf37fa21aab9
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-13 20:32:08 +0100

    Can solve example input

--- src/2018/13/main.py
+++ src/2018/13/main.py
@@ -2,8 +2,7 @@ from collections import namedtuple
 
 from util.geometry import Vector2, get_bounding_box
 
-Train = namedtuple('Train', 'position, direction, last_turn_direction')
-Rail = namedtuple('Rail', 'char, directions')
+Train = namedtuple('Train', 'id, position, direction, turn_count')
 
 
 class Directions:
@@ -14,11 +13,26 @@ class Directions:
 
 
 RAIL_DIRECTIONS_PER_CHAR = {
-    '/': {Directions.DOWN, Directions.RIGHT},
-    '\\': {Directions.RIGHT, Directions.DOWN},
-    '|': {Directions.UP, Directions.DOWN},
-    '-': {Directions.LEFT, Directions.RIGHT},
-    '+': {Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT}
+    '/': {
+        Directions.UP: Directions.RIGHT,
+        Directions.DOWN: Directions.LEFT,
+        Directions.LEFT: Directions.DOWN,
+        Directions.RIGHT: Directions.UP
+    },
+    '\\': {
+        Directions.UP: Directions.LEFT,
+        Directions.DOWN: Directions.RIGHT,
+        Directions.LEFT: Directions.UP,
+        Directions.RIGHT: Directions.DOWN
+    },
+    '|': {
+        Directions.UP: Directions.UP,
+        Directions.DOWN: Directions.DOWN
+    },
+    '-': {
+        Directions.LEFT: Directions.LEFT,
+        Directions.RIGHT: Directions.RIGHT
+    }
 }
 
 TRAIN_DIRECTIONS_PER_CHAR = {
@@ -40,6 +54,13 @@ def main():
     railroad, trains = parse_input([line.rstrip() for line in lines])
     draw_railroad(railroad, trains)
 
+    for i in range(15):
+        trains, crash_position = tick(railroad, trains)
+        if crash_position:
+            print('Crash: {}'.format(crash_position))
+            break
+        draw_railroad(railroad, trains)
+
 
 def parse_input(lines):
     railroad = {}
@@ -52,9 +73,9 @@ def parse_input(lines):
 
             position = Vector2(x, y)
             if c in TRAIN_DIRECTIONS_PER_CHAR.keys():
-                trains.append(Train(position, TRAIN_DIRECTIONS_PER_CHAR[c], None))
+                trains.append(Train(len(trains), position, TRAIN_DIRECTIONS_PER_CHAR[c], 0))
             else:
-                railroad[position] = Rail(c, RAIL_DIRECTIONS_PER_CHAR[c])
+                railroad[position] = c
 
     # Figure out rail pieces for positions that were covered by a train
     for train in trains:
@@ -65,19 +86,28 @@ def parse_input(lines):
         right = railroad.get(position + Directions.RIGHT)
 
         directions = set()
-        if up and up.char in '+/\\|':
+        if up and up in '+/\\|':
             directions.add(Directions.UP)
-        if down and down.char in '+/\\|':
+        if down and down in '+/\\|':
             directions.add(Directions.DOWN)
-        if left and left.char in '+/\\-':
+        if left and left in '+/\\-':
             directions.add(Directions.LEFT)
-        if right and right.char in '+/\\-':
+        if right and right in '+/\\-':
             directions.add(Directions.RIGHT)
 
-        for c, dirs in RAIL_DIRECTIONS_PER_CHAR.items():
-            if dirs == directions:
-                railroad[position] = Rail(c, directions)
-                break
+        if directions == {Directions.RIGHT, Directions.DOWN}:
+            railroad[position] = '/'
+        elif directions == {Directions.LEFT, Directions.UP}:
+            railroad[position] = '/'
+        elif directions == {Directions.UP, Directions.RIGHT}:
+            railroad[position] = '\\'
+        elif directions == {Directions.LEFT, Directions.DOWN}:
+            railroad[position] = '\\'
+        elif directions == {Directions.UP, Directions.DOWN}:
+            railroad[position] = '|'
+        else:
+            assert directions == {Directions.LEFT, Directions.RIGHT}
+            railroad[position] = '-'
 
     return railroad, trains
 
@@ -91,10 +121,39 @@ def draw_railroad(railroad, trains):
             if p in mapped_trains:
                 print(mapped_trains[p], end='')
             elif p in railroad:
-                print(railroad[p].char, end='')
+                print(railroad[p], end='')
             else:
                 print(' ', end='')
         print('')
+
+
+def tick(railroad, trains):
+    updated_trains = []
+    current_train_positions = {train.id: train.position for train in trains}
+
+    for train in sorted(trains, key=lambda t: (t.position.y, t.position.x)):
+        current_rail = railroad[train.position]
+        new_direction = None
+        turn_count = train.turn_count
+
+        if current_rail == CROSSING:
+            turn_count += 1
+            if train.turn_count % 3 == 0:
+                new_direction = Vector2(train.direction.y, -train.direction.x)
+            elif train.turn_count % 3 == 1:
+                new_direction = train.direction
+            elif train.turn_count % 3 == 2:
+                new_direction = Vector2(-train.direction.y, train.direction.x)
+        else:
+            new_direction = RAIL_DIRECTIONS_PER_CHAR[railroad[train.position]][train.direction]
+
+        new_position = train.position + new_direction
+        if new_position in current_train_positions.values():
+            return None, new_position
+        current_train_positions[train.id] = new_position
+        updated_trains.append(Train(train.id, new_position, new_direction, turn_count))
+
+    return updated_trains, None
 
 
 if __name__ == '__main__':
