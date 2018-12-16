3e9e41a1b1355244ff2906f6c981e493bf2a95c3
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-12-16 12:14:21 +0100

    Solved 2018 day 16 part 2

--- src/2018/16/main.py
+++ src/2018/16/main.py
@@ -1,3 +1,4 @@
+import operator
 from collections import namedtuple
 
 
@@ -54,6 +55,33 @@ def main():
     possible_opcodes_per_sample = [(sample, find_possible_opcodes(sample)) for sample in samples]
 
     print('Answer part 1: {}'.format(len([sample for sample, possible_opcodes in possible_opcodes_per_sample if len(possible_opcodes) >= 3])))
+    print('')
+
+    opcodes_per_possible_idx = {}
+    for sample, possible_opcodes in possible_opcodes_per_sample:
+        for possible_opcode in possible_opcodes:
+            opcodes_per_possible_idx.setdefault(sample.instruction[0], set()).add(possible_opcode)
+
+    print('Opcodes per potential idx:')
+    for idx, opcodes in opcodes_per_possible_idx.items():
+        print('{}: [{}]'.format(idx, ', '.join([opcode.name for opcode in opcodes])))
+    print('')
+
+    matched_idx_per_opcode = match_opcode_to_indices(opcodes_per_possible_idx)
+    opcode_per_idx = {idx: opcode for opcode, idx in matched_idx_per_opcode.items()}
+
+    print('Matched opcodes:')
+    for assigned_idx in sorted(opcode_per_idx.keys()):
+        print('{}: {}'.format(assigned_idx, opcode_per_idx[assigned_idx].name))
+    print()
+
+    program = load_program('input_test_program')
+    registers = [0] * REGISTER_COUNT
+    for instruction in program:
+        registers = opcode_per_idx[instruction[0]].execute(registers, instruction[1:])
+    print('Registers after running program: {}'.format(registers))
+
+    print('Answer part 2: {}'.format(registers[0]))
 
 
 def load_samples(filename):
@@ -73,7 +101,37 @@ def load_samples(filename):
 
 def find_possible_opcodes(sample):
     opcode_results = [(opcode, opcode.execute(sample.before, sample.instruction[1:])) for opcode in OPCODES]
-    return [opcode.name for opcode, after in opcode_results if after == sample.after]
+    return [opcode for opcode, after in opcode_results if after == sample.after]
+
+
+def match_opcode_to_indices(opcodes_per_possible_idx):
+    # Based on example here: https://www.geeksforgeeks.org/maximum-bipartite-matching/
+    matched_idx_per_opcode = {}
+    for idx in opcodes_per_possible_idx.keys():
+        match_found = match_idx_to_opcode(opcodes_per_possible_idx, idx, set(), matched_idx_per_opcode)
+        assert match_found
+
+    return matched_idx_per_opcode
+
+
+def match_idx_to_opcode(opcodes_per_possible_idx, current_idx, seen_opcodes, matched_idx_per_opcode):
+    for opcode in opcodes_per_possible_idx[current_idx]:
+        if opcode not in seen_opcodes:
+            seen_opcodes.add(opcode)
+
+            if opcode not in matched_idx_per_opcode or match_idx_to_opcode(opcodes_per_possible_idx, matched_idx_per_opcode[opcode], seen_opcodes, matched_idx_per_opcode):
+                matched_idx_per_opcode[opcode] = current_idx
+                return True
+
+    return False
+
+
+def load_program(filename):
+    print('Loading program: {}'.format(filename))
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines() if len(line.strip()) > 0]
+
+    return [[int(n) for n in line.split()] for line in lines]
 
 
 if __name__ == '__main__':
